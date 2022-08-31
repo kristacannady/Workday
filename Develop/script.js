3c360d3c2b60e308d8b2d78a351c43c12210d63b
Krista Cannady <kristalcannady@gmail.com>
2022-08-31 15:48:40 -0400

    add times

---
+++ Develop/script.js
@@ -0,0 +1,32 @@
+
+// Display the date at the top
+var currentDay = moment().format('dddd, MMMM Do, YYYY');
+$("#currentDay").text(currentDay);
+
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+      setColor(row, "red");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+      setColor(row, "green");
+    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+      setColor(row, "lightgrey");
+    } else {
+      setColor(row, "white");
+    }
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
